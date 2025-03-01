e8867372f771da36a3cee4cfa79f98e7e3b97a7f
tikorajasigma <ticohalamadrid@gmail.com>
2025-03-01 21:29:01 +0800

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const chalk = require('chalk');
+
+global.owner = ['6289530285149']
+global.packname = 'Tiko Bot'
+global.author = 'Tiko Bot'
+global.botname = 'Tiko Bot'
+global.listprefix = ['+','!','.','/']
+global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
+global.tempatDB = 'database.json'
+global.pairing_code = true
+global.number_bot = '6281299633651'
+
+global.fake = {
+	img: 'https://raw.githubusercontent.com/tikorajasigma/tikobot/refs/heads/main/tikobot.jpg',
+	thumbnailUrl: 'https://telegra.ph/file/fe4843a1261fc414542c4.jpg',
+	thumbnail: fs.readFileSync('./src/media/naze.png'),
+	docs: fs.readFileSync('./src/media/fake.pdf'),
+	listfakedocs: ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','application/vnd.openxmlformats-officedocument.presentationml.presentation','application/vnd.openxmlformats-officedocument.wordprocessingml.document','application/pdf'],
+}
+
+global.my = {
+	yt: 'https://youtube.com/c/Nazedev',
+	gh: 'https://github.com/nazedev',
+	gc: 'https://chat.whatsapp.com/B5qJIwZHm4VEYZJQE6iMwy',
+	ch: '120363250409960161@newsletter',
+}
+
+global.limit = {
+	free: 10,
+	premium: 50,
+	vip: 100
+}
+
+global.uang = {
+	free: 10,
+	premium: 500,
+	vip: 1000
+}
+
+global.mess = {
+	key0: 'Apikey mu telah habis silahkan kunjungi\nhttps://my.hitori.pw',
+	owner: 'Fitur Khusus Owner!',
+	admin: 'Fitur Khusus Admin!',
+	botAdmin: 'Bot Bukan Admin!',
+	group: 'Gunakan Di Group!',
+	private: 'Gunakan Di Privat Chat!',
+	limit: 'Limit Anda Telah Habis!',
+	prem: 'Fitur Khusus Premium!',
+	wait: 'Loading',
+	error: 'Error!',
+	done: 'Done'
+}
+
+global.APIs = {
+	hitori: 'https://my.hitori.pw/api',
+}
+global.APIKeys = {
+	'https://my.hitori.pw/api': 'htrkey-awokawok',
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+});
